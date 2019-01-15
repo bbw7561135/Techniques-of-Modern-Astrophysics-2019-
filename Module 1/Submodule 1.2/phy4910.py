f2d862309468f17111a8c6d1185bfe626261116f
Joseph MacMillan <joseph.macmillan@uoit.ca>
2019-01-15 14:03:09 -0500

    Fixed problem with library

--- Module 1/Submodule 1.2/phy4910.py
+++ Module 1/Submodule 1.2/phy4910.py
@@ -1,8 +1,8 @@
 import numpy as np
-import phy4910
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib import rc
+
 rc('text', usetex=True)
 matplotlib.rcParams['mathtext.fontset'] = 'cm'
 matplotlib.rcParams['font.family'] = 'STIXGeneral'
@@ -10,32 +10,103 @@ plt.rcParams.update({'font.size':14})
 plt.rc('axes', labelsize=16)
 plt.rcParams.update({'figure.autolayout': True})
 
-
-
-
-n = 1.0
-
-def f(x, y, z):
-    return z;
+"""
+Plot with reasonable settings
+"""
+def plot(x, f, xlabel="", ylabel=""):
+        
+    fig = plt.figure(figsize=(6, 4))
+    ax = fig.add_subplot(1,1,1)
+    ax.grid(True)
+    ax.plot(x, f, color="black")
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    plt.show()
     
-def g(x, y, z):
-    return -2.0 / x * z - np.power(y, n)
+"""
+Print the plot with reasonable settings
+"""
+def plot_print(x, f, filename, xlabel="", ylabel=""):
+        
+    fig = plt.figure(figsize=(4, 3), dpi=200)
+    ax = fig.add_subplot(1,1,1)
+    ax.plot(x, f, color="black")
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    plt.savefig(filename)
+    
+    
+"""
+Solves a coupled pair of ODEs using euler
+ 
+Takes as arguments:
+  x_start - starting point for independent coordinate
+  x_end - ending point
+  h - difference between x_i and x_i+1 (i.e., delat x)
+  y0 - initial value for first variable y(x_start)
+  z_0 - initial value for second variable z(x_start)
+  f - function for derivative of first variable (i.e., f = dy/dx)
+  g - function for deriviative of second variable (i.e., g = dz/dx)
+    
+  returns three arrays, x[0,N-1], y[0,N-1], and z[0,N-1].
+"""
+def ode_euler(x_start, x_end, h, y0, z0, f, g):
+    
+    x = np.arange(x_start, x_end, h)
+    N = len(x)
+    y = np.zeros(N)
+    y[0] = y0
+    z = np.zeros(N)
+    z[0] = z0
+    
+    for i in range(0, N-1):
+        k1 = h * f(x[i], y[i], z[i])
+        l1 = h * g(x[i], y[i], z[i])
+            
+        y[i+1] = y[i] + k1
+        z[i+1] = z[i] + l1
+                
+    return x, y, z
+    
 
-eta1, rho1, tmp = phy4910.ode_euler(0.00001, 10, 0.3, 1.0, 0.0, f, g)
+""" 
 
-eta2, rho2, tmp = phy4910.ode_rk4(0.00001, 10, 0.3, 1.0, 0.0, f, g)
+Solves a coupled pair of ODEs using runge kutta.
+ 
+Takes as arguments:
+  x_start - starting point for independent coordinate
+  x_end - ending point
+  h - difference between x_i and x_i+1 (i.e., delat x)
+  y0 - initial value for first variable y(x_start)
+  z_0 - initial value for second variable z(x_start)
+  f - function for derivative of first variable (i.e., f = dy/dx)
+  g - function for deriviative of second variable (i.e., g = dz/dx)
+  stop - a function for a stopping criteria.  Function must take three number (xi, yi, zi).
+  data - any data that needs to be passed to the stop function in addition to xi, yi, zi.
+  
+  returns three arrays, x[0,N-1], y[0,N-1], and z[0,N-1].
+"""
 
-fig = plt.figure(figsize=(6, 4))
-ax = fig.add_subplot(1,1,1)
-ax.grid(True)
-
-ax.plot(eta1, rho1, color="black")
-ax.plot(eta2, rho2, color="red")
-ax.plot(eta1, np.sin(eta1)/eta1, color="blue", linewidth=0.7)
-#ax.plot(eta2, rho2-rho1, color="red")
-
-
-plt.show()
-
-
-
+def ode_rk4(x_start, x_end, h, y0, z0, f, g):
+    
+    x = np.arange(x_start, x_end, h)
+    N = len(x)
+    y= np.zeros(N)
+    y[0] = y0
+    z = np.zeros(N)
+    z[0] = z0
+    
+    for i in range(0, N-1):
+        k1 = h * f(x[i], y[i], z[i])
+        l1 = h * g(x[i], y[i], z[i])
+        k2 = h * f(x[i] + 0.5 * h, y[i] + 0.5 * k1, z[i] + 0.5 * l1)
+        l2 = h * g(x[i] + 0.5 * h, y[i] + 0.5 * k1, z[i] + 0.5 * l1)
+        k3 = h * f(x[i] + 0.5 * h, y[i] + 0.5 * k2, z[i] + 0.5 * l2)
+        l3 = h * g(x[i] + 0.5 * h, y[i] + 0.5 * k2, z[i] + 0.5 * l2)
+        k4 = h * f(x[i] + h, y[i] + k3, z[i] + l3)
+        l4 = h * g(x[i] + h, y[i] + k3, z[i] + l3)
+    
+        y[i+1] = y[i] + (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0
+        z[i+1] = z[i] + (l1 + 2.0 * l2 + 2.0 * l3 + l4) / 6.0
+        
+    return x, y, z
