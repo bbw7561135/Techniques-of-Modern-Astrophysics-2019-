3567a7ab17e5473397bb845dbc97e2983cd48b58
wbarber1 <william.barber1@uoit.net>
2019-01-22 16:37:21 -0500

    updated jan 22 fixes

--- Module 1/Submodule 1.2/lane-emden.py
+++ Module 1/Submodule 1.2/lane-emden.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy as sp
+import scipy.integrate
 import phy4910
 import matplotlib
 import matplotlib.pyplot as plt
@@ -11,38 +12,38 @@ plt.rcParams.update({'font.size':14})
 plt.rc('axes', labelsize=16)
 plt.rcParams.update({'figure.autolayout': True})
 
-
-
+stepsize = 0.001
+filename = "assignment1a1.pdf"
 
 n = 1.5
 
 def f(x, y, z):
     return z;
-    
+
 def g(x, y, z):
     return -2.0 / x * z - np.power(y, n)
 
-eta1, rho1, tmp = phy4910.ode_euler(0.00001, 10, 0.3, 1.0, 0.0, f, g)
+eta1, rho1, tmp = phy4910.ode_euler(0.00001, 10, stepsize, 1.0, 0.0, f, g)
 
-eta2, rho2, tmp = phy4910.ode_rk4(0.00001, 10, 0.3, 1.0, 0.0, f, g)
+eta2, rho2, tmp = phy4910.ode_rk4(0.00001, 10, stepsize, 1.0, 0.0, f, g)
 
-fig = plt.figure(figsize=(6, 4))
-ax = fig.add_subplot(1,1,1)
-ax.grid(True)
+# fig = plt.figure(figsize=(6, 4))
+# ax = fig.add_subplot(1,1,1)
+# ax.grid(True)
 
 #ax.plot(eta1, rho1, color="black")
-ax.plot(eta2, rho2, color="red")
+# ax.plot(eta2, rho2, color="red")
 #ax.plot(eta1, np.sin(eta1)/eta1, color="blue", linewidth=0.7)
 #ax.plot(eta2, rho2-rho1, color="red")
 
-plt.savefig("assignment1a1.pdf")
+# plt.savefig("assignment1a1.pdf")
 
-#plt.show()
+# plt.show()
+
+phy4910.plot_print(eta2, rho2, filename, "$\\eta$", "$\\rho(\\eta)$")
 
 varrho = np.power(rho2[rho2 > 0],n) * np.power(eta2[rho2 > 0],2)
 
-m = sp.integrate.trapz(varrho,x)
-print(m)
-
-
-
+m = sp.integrate.trapz(varrho,eta2[rho2 > 0])
+print("Eta_s: {}".format(max(eta2[rho2 >0])))
+print("Mass: {}".format(m))
