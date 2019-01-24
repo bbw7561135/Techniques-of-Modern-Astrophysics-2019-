d88a26fabc85df034dc2064b9144b7c86a4a0069
wbarber1 <william.barber1@uoit.net>
2019-01-24 13:30:12 -0500

    final code updates part c

--- Module 1/Submodule 1.2/lane-emden.py
+++ Module 1/Submodule 1.2/lane-emden.py
@@ -18,6 +18,8 @@ filename2 = "assignment1b1-2.pdf"
 filename3 = "assignment1c3.pdf"
 
 n = 3.0
+N = 25
+rho0 = 3.789e6
 
 # k = 3.166e12
 k = 4.936e14
@@ -58,8 +60,25 @@ eta2, rho2, tmp = phy4910.ode_rk4(0.00001, 10, stepsize, 1.0, 0.0, f, g)
 
 # plt.show()
 
-phy4910.plot_print(eta2, rho2, filename, "$\\eta$", "$\\rho(\\eta)$")
+# phy4910.plot_print(eta2, rho2, filename, "$\\eta$", "$\\rho(\\eta)$")
+#
+# varrho = np.power(rho2[rho2 > 0],n) * np.power(eta2[rho2 > 0],2)
+#
+# m = sp.integrate.trapz(varrho,eta2[rho2 > 0])
+# print("Eta_s: {}".format(max(eta2[rho2 > 0])))
+# print("Mass: {}".format(m))
+#
+# rho_c = (M / (4 * np.pi * m)) ** 2 * ((5 * k) / (8 * np.pi * G)) ** -3
+# print("Rho_c: {}".format(rho_c))
+#
+# lambdan = (((n+1) * k * rho_c ** ((1-n)/n))/(4*np.pi*G)) ** 0.5
+# r = (lambdan*eta2[rho2 > 0]) / 695e8
+# phy4910.plot_print(r, rho_c * rho2[rho2 > 0] / 1e6, filename2, "Radius (solar radii)", "Density ($10^6g/cm^3$)")
+# print("Radius: {}".format(max(r)))
 
+eta2, rho2, tmp = phy4910.ode_rk4(0.00001, 10, stepsize, 1.0, 0.0, f, rnr_g)
+
+phy4910.plot_print(eta2, rho2, filename3, "$\\eta$", "$\\varrho(\\eta)$")
 varrho = np.power(rho2[rho2 > 0],n) * np.power(eta2[rho2 > 0],2)
 
 m = sp.integrate.trapz(varrho,eta2[rho2 > 0])
@@ -69,11 +88,31 @@ print("Mass: {}".format(m))
 rho_c = (M / (4 * np.pi * m)) ** 2 * ((5 * k) / (8 * np.pi * G)) ** -3
 print("Rho_c: {}".format(rho_c))
 
-lambdan = (((n+1) * k * rho_c ** ((1-n)/n))/(4*np.pi*G)) ** 0.5
-r = (lambdan*eta2[rho2 > 0]) / 695e8
-phy4910.plot_print(r, rho_c * rho2[rho2 > 0] / 1e6, filename2, "Radius (solar radii)", "Density ($10^6g/cm^3$)")
-print("Radius: {}".format(max(r)))
+rhoCR = np.logspace(4,12,N)
+#eta_array = np.empty(N)
+#varrho_array = np.empty(N)
+#sigma_array = np.empty(N)
+mass = np.empty(N)
+eta_array = []
+varrho_array = []
+sigma_array = []
+radii = []
+lambda2 = np.sqrt(k/(4*np.pi*G*np.power(rho0,1.0/3.0)))
 
-eta2, rho2, tmp = phy4910.ode_rk4(0.00001, 10, stepsize, 1.0, 0.0, f, rnr_g)
+for i in range(N):
+    x,y,z = phy4910.ode_rk4(0.00001, 10, stepsize, rhoCR[i] / rho0, 0.0, f, rnr_g)
+    eta_array.append(x)
+    varrho_array.append(y)
+    sigma_array.append(z)
+    varrho = np.power(varrho_array[i],n) * np.power(eta_array[i],2)
+    mass[i] = sp.integrate.trapz(varrho,eta_array[i])/2.0
+    radii.append(max(y) * lambda2 / 695e12)
 
-phy4910.plot_print(eta2, rho2, filename3, "$\\eta$", "$\\varrho(\\eta)$")
+proper_mass = (mass[i] * (rhoCR/rho0)**(0.5) * 4 * np.pi * (5 * k /(8*np.pi*G) ) ** (3.0/2.0)) * 1e-16
+
+phy4910.plot(rhoCR, proper_mass/2e30, "Central Density", "Solar Masses")
+print("Stable mass: {}".format(max(proper_mass/2e30)))
+
+phy4910.plot(rhoCR, radii, "Central Density", "Solar Radii/100")
+
+phy4910.plot(proper_mass/2e30,radii, "Solar Masses", "Solar Radii/100")
