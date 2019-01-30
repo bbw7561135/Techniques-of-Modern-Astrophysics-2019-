ca7c757c4f04b2807b67d7a05700614df864c5da
Joseph MacMillan <joseph.macmillan@uoit.ca>
2019-01-30 13:13:05 -0500

    Add clean version of phy4910.py

---
+++ Module 1/Submodule 1.6/phy4910.py
@@ -0,0 +1,75 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
+rc('text', usetex=True)
+matplotlib.rcParams['mathtext.fontset'] = 'cm'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+plt.rcParams.update({'font.size':14})
+plt.rc('axes', labelsize=16)
+plt.rcParams.update({'figure.autolayout': True})
+
+def plot(x, f, xlabel="", ylabel=""):
+        
+    fig = plt.figure(figsize=(6, 4))
+    ax = fig.add_subplot(1,1,1)
+    ax.grid(True)
+    ax.plot(x, f, color="black")
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    plt.show()
+    
+
+def plot_print(x, f, filename, xlabel="", ylabel=""):
+        
+    fig = plt.figure(figsize=(6, 4))
+    ax = fig.add_subplot(1,1,1)
+    ax.plot(x, f, color="black")
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    plt.savefig(filename)
+    
+
+def ode_euler(x_start, x_end, h, y0, z0, f, g):
+    
+    x = np.arange(x_start, x_end, h)
+    N = len(x)
+    y = np.zeros(N)
+    y[0] = y0
+    z = np.zeros(N)
+    z[0] = z0
+    
+    for i in range(0, N-1):
+        k1 = h * f(x[i], y[i], z[i])
+        l1 = h * g(x[i], y[i], z[i])
+            
+        y[i+1] = y[i] + k1
+        z[i+1] = z[i] + l1
+                
+    return x, y, z
+    
+def ode_rk4(x_start, x_end, h, y0, z0, f, g):
+    
+    x = np.arange(x_start, x_end, h)
+    N = len(x)
+    y= np.zeros(N)
+    y[0] = y0
+    z = np.zeros(N)
+    z[0] = z0
+    
+    for i in range(0, N-1):
+        k1 = h * f(x[i], y[i], z[i])
+        l1 = h * g(x[i], y[i], z[i])
+        k2 = h * f(x[i] + 0.5 * h, y[i] + 0.5 * k1, z[i] + 0.5 * l1)
+        l2 = h * g(x[i] + 0.5 * h, y[i] + 0.5 * k1, z[i] + 0.5 * l1)
+        k3 = h * f(x[i] + 0.5 * h, y[i] + 0.5 * k2, z[i] + 0.5 * l2)
+        l3 = h * g(x[i] + 0.5 * h, y[i] + 0.5 * k2, z[i] + 0.5 * l2)
+        k4 = h * f(x[i] + h, y[i] + k3, z[i] + l3)
+        l4 = h * g(x[i] + h, y[i] + k3, z[i] + l3)
+    
+        y[i+1] = y[i] + (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0
+        z[i+1] = z[i] + (l1 + 2.0 * l2 + 2.0 * l3 + l4) / 6.0
+        
+    return x, y, z
+        
